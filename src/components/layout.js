9a67675942255bda1824420b75ca08d7fa5383ee
David Navarro <davidnavarro@Davids-MacBook-Pro-2.local>
2019-04-26 18:34:27 -0700

    Remove Netlify Forms

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+// import 'bootstrap-css-only/css/bootstrap.min.css';
+// import 'mdbreact/dist/css/mdb.css';
 
 import { rhythm, scale } from "../utils/typography"
 
