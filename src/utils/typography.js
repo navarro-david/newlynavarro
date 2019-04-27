b0e678a9aa5b14785195b8cceee692f341768e4c
David Navarro <davidnavarro@Davids-MacBook-Pro-2.local>
2019-04-26 18:51:15 -0700

    Overriding some typography styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,18 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import doelgerTheme from 'typography-theme-doelger'
 
-Wordpress2016.overrideThemeStyles = () => {
+doelgerTheme.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
     },
+    "a:link":{
+      backgroundImage: `none`
+    }
   }
 }
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(doelgerTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
