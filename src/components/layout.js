527d2b687549623e95566d16500db562809922e4
David Navarro <davidnavarro@Davids-MacBook-Pro-2.local>
2019-04-26 21:18:03 -0700

    Added Google Fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,9 @@ import { Link } from "gatsby"
 
 import "./layout.scss"
 
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import 'bootstrap-css-only/css/bootstrap.min.css';
-// import 'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 import { rhythm, scale } from "../utils/typography"
 
